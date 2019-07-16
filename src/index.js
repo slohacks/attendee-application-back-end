62e305549986a1fb32f998d3072149919d117f99
Michael Tran <michaeltran3599@gmail.com>
2019-07-16 16:20:45 -0700

    feat(User.js, user.js, index.js): Added login/signup routes and userSchema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
+const userRouter = require('./routers/user.js')
 const morgan = require('morgan')
 const app = express()
 const port = 4000
 
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(userRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
