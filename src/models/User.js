524537211100334023c133043da1a764d9290397
michaeltran3599 <33034465+michaeltran3599@users.noreply.github.com>
2019-07-16 21:10:03 -0700

    feat(User.js, user.js, index.js): Added login/signup routes and userSchema (#5)

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,64 @@
-// User Model Here
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate (value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    trim: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  resetPasswordKey: {
+    type: String,
+    default: null
+  },
+  verifyTime: {
+    type: String
+  },
+  resetPasswordTime: {
+    type: String
+  }
+})
+
+// Finds email in database and compared the given password to the hashed password in the database
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+  return user
+}
+
+// Hashes password before saving
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,1 +1,26 @@
-// Users Routes Here
+const express = require('express')
+const User = require('../models/user')
+const router = new express.Router()
+
+// Post request to add user to database. Expects all required fields in User Schema
+router.post('/users/signup', async (req, res) => {
+  const user = new User(req.body)
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+// Post request to login a user. Takes in email and password and checks if it matches hashed password
+router.get('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    res.send(user)
+  } catch (e) {
+    res.status(400).send()
+  }
+})
+
+module.exports = router
