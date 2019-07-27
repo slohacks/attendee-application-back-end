6b6ec5634cec85cb0c8d71aacc3dc051b8af0199
Christopher Specht <45947147+cfspecht@users.noreply.github.com>
2019-07-26 18:36:20 -0700

    feat(/models /routers): add application model and routes (#7)

--- src/models/Application.js
+++ src/models/Application.js
@@ -1,1 +1,123 @@
-// Application Model Here
+// import modules
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+// class definition
+const Application = mongoose.model('Application', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: null
+  },
+  gender: {
+    type: String,
+    required: true,
+    min: 0,
+    max: 4
+  },
+  major: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  mlh: {
+    type: Boolean,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    validate (value) {
+      if (!validator.isMobilePhone(value)) {
+        throw new Error('Phone number is invalid')
+      }
+    }
+  },
+  timestamp: {
+    type: Date,
+    required: true
+  },
+  grad_date: { // dateInput?
+    // mongoose stores dates in the format "1987-09-28"
+    // potentially just store grad year
+    // potentially delete this field
+    type: Number,
+    required: true,
+    trim: true
+  },
+  grade: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  allergies: {
+    type: String,
+    trim: true
+  },
+  college: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  ethnicity: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 6
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Boolean,
+    required: true
+  },
+  github: {
+    type: String,
+    trim: true
+  },
+  linkedin: {
+    type: String,
+    trim: true
+  },
+  website: {
+    type: String,
+    trim: true
+  },
+  diet: {
+    type: Number,
+    min: 0,
+    max: 4
+  },
+  misc: {
+    type: String,
+    trim: true
+  },
+  project: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  challenge: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+// export Application model
+module.exports = Application
+
+// NOTES
+//  molter for resume field
+//  blacklist things like "".:)(/" to clean textInput?
+//  lowercase for text input?
--- src/routers/application.js
+++ src/routers/application.js
@@ -1,10 +1,47 @@
+// import modules
 const express = require('express')
+const Application = require('../models/Application')
 const router = new express.Router()
 const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/applications', authMiddleware, (req, res) => {
+// Post request to add new application to database
+router.post('/applications', authMiddleware, async (req, res) => {
   console.log(req.user, req.token)
   res.status(200).send({ message: 'Successful Authentication' })
+  const application = new Application(req.body)
+  application.owner = req.user._id
+
+  // intermediate functions here
+  //  lowercase?
+  //  whitelist?
+
+  try {
+    // check if application for user already exists
+    const application = await Application.findOne({ owner: req.user._id })
+    if (application) {
+      throw new Error('Application for this user already exists')
+      // possibly return a response with error code other than 400
+    }
+    await application.save()
+    res.status(201).send(application)
+  } catch (err) {
+    res.status(400).send({ errorMessage: err.message })
+  }
+})
+
+// Get request to fetch a user's application based on user's ID
+router.get('/applications/:id', authMiddleware, async (req, res) => {
+  const appId = req.params.id
+  try {
+    // find application based on application objectID and current user's objectID
+    const application = await Application.findOne({ appId, owner: req.user._id })
+    if (!application) {
+      return res.status(404).send()
+    }
+    res.status(200).send(application)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
 module.exports = router
