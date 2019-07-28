ada41508f6bfce440cb53779323c7e0c355f767f
Jumer-Caragay <jumer.caragay@gmail.com>
2019-07-27 20:31:52 -0700

    fix(all): fix code syntax and renaming conventions

--- src/routers/application.js
+++ src/routers/application.js
@@ -1,26 +1,16 @@
 // import modules
 const express = require('express')
-const Application = require('../models/Application')
+const Application = require('../models/ApplicationModel')
 const router = new express.Router()
 const authMiddleware = require('../middleware/authMiddleware')
 
-// Post request to add new application to database
 router.post('/applications', authMiddleware, async (req, res) => {
-  console.log(req.user, req.token)
-  res.status(200).send({ message: 'Successful Authentication' })
-  const application = new Application(req.body)
-  application.owner = req.user._id
-
-  // intermediate functions here
-  //  lowercase?
-  //  whitelist?
-
   try {
-    // check if application for user already exists
-    const application = await Application.findOne({ owner: req.user._id })
-    if (application) {
+    const application = new Application(req.body)
+    application.owner = req.user._id
+    const existingApplication = await Application.findOne({ owner: req.user._id })
+    if (existingApplication) {
       throw new Error('Application for this user already exists')
-      // possibly return a response with error code other than 400
     }
     await application.save()
     res.status(201).send(application)
@@ -29,18 +19,16 @@ router.post('/applications', authMiddleware, async (req, res) => {
   }
 })
 
-// Get request to fetch a user's application based on user's ID
 router.get('/applications/:id', authMiddleware, async (req, res) => {
-  const appId = req.params.id
   try {
-    // find application based on application objectID and current user's objectID
-    const application = await Application.findOne({ appId, owner: req.user._id })
+    const appId = req.params.id
+    const application = await Application.findOne({ _id: appId, owner: req.user._id })
     if (!application) {
-      return res.status(404).send()
+      return res.status(404).send({})
     }
     res.status(200).send(application)
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send({ errorMessage: err.message })
   }
 })
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const User = require('../models/User')
+const User = require('../models/UserModel')
 const router = new express.Router()
 
 // Post request to add user to database. Expects all required fields in User Schema
