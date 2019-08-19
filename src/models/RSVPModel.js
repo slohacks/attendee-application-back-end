36405327b96f8ea1169150b5d70dbd55a8440194
Michael Tran <michaeltran3599@gmail.com>
2019-08-19 14:12:39 -0700

    feat(index.js, RSVPModel.js, rsvp.js): add rsvp model and routes
    add rsvp model and routes to create and get an authenticated user's rsvp

---
+++ src/models/RSVPModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const RSVP = mongoose.model('RSVP', {
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  attending: {
+    type: Boolean,
+    required: true
+  },
+  shirt: {
+    type: Number,
+    min: 0,
+    max: 4,
+    required: function() {return this.attending}
+
+  },
+  travelType: {
+    type: Number,
+    min: 0,
+    max: 2,
+    required: function() {return this.attending}
+  },
+  bus: {
+    type: Number,
+    min: 0,
+    max: 2,
+    required: function() {return this.attending && this.travelType === 0}
+  },
+  socal: {
+    type: Number,
+    min: 0,
+    max: 2,
+    required: function() {return this.attending && this.bus === 0}
+  },
+  norcal: {
+    type: Number,
+    min: 0,
+    max: 2,
+    required: function() {return this.attending && this.bus === 1}
+    
+  },
+  flight: {
+    type: String,
+    required: function() {return this.attending && this.travelType === 1}
+  },
+  misc: {
+    type: String
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = RSVP
