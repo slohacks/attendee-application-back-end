5840cc0ebd58d798134c5282175a589be982ee19
Jumer-Caragay <jumer.caragay@gmail.com>
2019-09-09 01:37:13 -0700

    feat(all): create first iteration of test suite

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const applicationRouter = require('./routers/application')
-const emailRequestRouter = require('./routers/emailRequest')
-const resumeRouter = require('./routers/resume')
-const forgotPasswordRequestRouter = require('./routers/forgotPasswordRequest')
-const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
+
 const port = process.env.PORT
-
-app.use(cors())
-
-app.use(express.json())
-app.use(morgan('combined'))
-app.use(userRouter)
-app.use(applicationRouter)
-app.use(emailRequestRouter)
-app.use(forgotPasswordRequestRouter)
-app.use(resumeRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
